54ce9e48c578f6202e7487883393c642f6bd388f
Shama <assiddik001@gmail.com>
2022-01-25 22:08:02 +0600

    M17 V07 - String Concatenation

---
+++ Milestone_04 - Javascript/Module_17 - Introductions to JS/07_(advanced) String Concatenation, Integer float parseInt parseFloat type conversion/string_convertions.js
@@ -0,0 +1,20 @@
+var firstName = "Abu";
+var lastName = "Shama";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+var number1 = "23";
+var number2 = "20.6";
+
+
+var integerNumber1 = parseInt(number1);
+var integerNumber2 = parseFloat(number2);
+
+var totalNumber = number1 + number2;
+console.log(totalNumber);
+
+
+totalIntegerNumber = integerNumber1 + integerNumber2;
+console.log(totalIntegerNumber);
