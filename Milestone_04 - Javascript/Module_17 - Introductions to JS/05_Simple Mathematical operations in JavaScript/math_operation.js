6774619dfbde8d2a23d64397ac738b1942ce0106
Shama <assiddik001@gmail.com>
2022-01-25 21:35:38 +0600

    M17 V05 - Mathematical operations

---
+++ Milestone_04 - Javascript/Module_17 - Introductions to JS/05_Simple Mathematical operations in JavaScript/math_operation.js
@@ -0,0 +1,33 @@
+// Jog
+var banglaNumber = 67;
+var englishNumber = 32;
+
+var totalNumber = banglaNumber + englishNumber;
+
+console.log(totalNumber);
+
+// Biyog
+var correctAnswer = 13;
+var wrongAnswer = 7;
+
+var mark = correctAnswer - wrongAnswer;
+
+console.log(mark);
+
+// Guun
+
+var egg = 7.5;
+var quantity = 7;
+
+var eggPrice = egg * quantity;
+
+console.log(eggPrice);
+
+// Vag
+
+var money = 200;
+var orange = 13;
+
+var totalOrange = money / orange;
+
+console.log(totalOrange);
