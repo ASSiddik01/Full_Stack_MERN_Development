6c608412be302e2092986980c71d77fb970e60f7
Shama <assiddik001@gmail.com>
2022-01-26 22:00:40 +0600

    M18 V06 - if-else coditional comparision

---
+++ Milestone_04 - Javascript/Module_18 - Array and conditions/06_Make conditional decision, if-else, comparison/conditions.js
@@ -0,0 +1,8 @@
+var iphonePrice = 93000;
+var myBudgets = 40000;
+
+if (iphonePrice < myBudgets) {
+    console.log('iPhone kinbo mama');
+} else {
+    console.log('Oppo phone daw mama oto taka nai pocket a')
+}
