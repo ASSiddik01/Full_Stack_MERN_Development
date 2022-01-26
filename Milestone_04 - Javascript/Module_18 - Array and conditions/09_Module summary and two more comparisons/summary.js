a047b272e8c0c2e0089a7154b61d94cd8abe202a
Shama <assiddik001@gmail.com>
2022-01-26 22:42:43 +0600

    M18 V09 - Module summary

---
+++ Milestone_04 - Javascript/Module_18 - Array and conditions/09_Module summary and two more comparisons/summary.js
@@ -0,0 +1,14 @@
+var myName = "Shama";
+
+var studentsName = ['Shama', 'Sunny', 'Sweet', 'Sakib']
+var studentsLength = studentsName.length // get lenght
+
+studentsName.push('Sadia'); // add element
+
+studentsName.pop(); // remove element
+
+// Comparision
+
+// Condition
+
+// nested condition
