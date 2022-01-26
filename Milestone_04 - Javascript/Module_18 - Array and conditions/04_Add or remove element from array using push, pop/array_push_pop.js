15c3c0e030cb176be838805dbc11df28aa777e3d
Shama <assiddik001@gmail.com>
2022-01-26 21:30:47 +0600

    M18 V04 - Add or remove element from array

---
+++ Milestone_04 - Javascript/Module_18 - Array and conditions/04_Add or remove element from array using push, pop/array_push_pop.js
@@ -0,0 +1,17 @@
+var friendsName = ["Shama", "Sakib", "Sunny"];
+
+// Add element from last
+friendsName.push('Sadia');
+console.log(friendsName);
+
+// Add element from first
+// friendsName.unshift('Sweet');
+// console.log(friendsName);
+
+// Remove element from first
+// friendsName.pop();
+// console.log(friendsName);
+
+// Remove element from first
+friendsName.shift();
+console.log(friendsName);
