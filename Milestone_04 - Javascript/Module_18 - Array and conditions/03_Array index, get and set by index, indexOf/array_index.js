0d7354ceb02cf56cb5a2ade6d9d6d9a862b40b17
Shama <assiddik001@gmail.com>
2022-01-26 21:12:15 +0600

    M18 V03 - indexOf, get and set

---
+++ Milestone_04 - Javascript/Module_18 - Array and conditions/03_Array index, get and set by index, indexOf/array_index.js
@@ -0,0 +1,17 @@
+var friendsName = ["Shama", "Sakib", "Sunny"];
+
+console.log(friendsName);
+
+var sunnyIndex = friendsName.indexOf('Sunny');
+
+// console.log(sunnyIndex);
+
+var firstPosition = friendsName[1];
+
+// console.log(firstPosition);
+
+// Set Element
+
+friendsName[2] = "Shofik";
+
+console.log(friendsName);
