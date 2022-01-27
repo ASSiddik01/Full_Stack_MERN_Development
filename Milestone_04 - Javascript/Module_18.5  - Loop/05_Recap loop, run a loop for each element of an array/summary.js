15db8b5cde7836bed036238dc263ee3ff52c3e5d
Shama <assiddik001@gmail.com>
2022-01-27 21:26:20 +0600

    M18.5 V05 - For Loop on array

---
+++ Milestone_04 - Javascript/Module_18.5  - Loop/05_Recap loop, run a loop for each element of an array/summary.js
@@ -0,0 +1,20 @@
+// i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i += 2;
+// }
+
+
+// for (var i = 0; i <= 10; i += 2) {
+//     console.log(i)
+// }
+
+
+console.log('Loop in array');
+
+var names = ['Shama', 'Shakib', 'Sweet', 'Sanny', 'Sadia'];
+
+for (var i = 0; i < names.length; i++) {
+    var name = names[i];
+    console.log(name);
+}
