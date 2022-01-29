63a9d407b82634e0f66266687bbd44b8108b8c53
Shama <assiddik001@gmail.com>
2022-01-29 16:07:11 +0600

    M19 V06 - Multiple ways to get and set object

---
+++ Milestone_04 - Javascript/Module_19 - Function and Object/06_multiple ways to get and set object property/object.js
@@ -0,0 +1,15 @@
+var student1 = {
+    id: 04,
+    name: 'Shama',
+    age: 34,
+    phone: '01292901827'
+}
+
+// console.log(student1.phone);
+
+// set property value
+// student1.age = student1.age + 1;
+var studentAge = "age"
+student1[studentAge] = 44;
+
+console.log(student1);
