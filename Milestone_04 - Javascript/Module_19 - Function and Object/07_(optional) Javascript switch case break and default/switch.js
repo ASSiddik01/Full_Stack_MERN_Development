7e60678e20f2a9e23041023907a05020cf4adbdb
Shama <assiddik001@gmail.com>
2022-01-29 16:19:31 +0600

    M19 V07 - Javascript switch case break and default

---
+++ Milestone_04 - Javascript/Module_19 - Function and Object/07_(optional) Javascript switch case break and default/switch.js
@@ -0,0 +1,13 @@
+color = 'ddd'
+
+switch (color) {
+    case "red":
+        console.log('Your color is', color);
+        break;
+    case "black":
+        console.log('Your color is', color);
+        break;
+    default:
+        console.log('Your color is not match');
+        break;
+}
