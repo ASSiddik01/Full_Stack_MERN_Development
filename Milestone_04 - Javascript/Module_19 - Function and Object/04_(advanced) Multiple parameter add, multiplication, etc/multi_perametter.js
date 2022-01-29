79d77fa0bdbd6698f3cc0c97b6794bba91c6a0d9
Shama <assiddik001@gmail.com>
2022-01-29 15:37:24 +0600

    M19 V04 - Multiple parameter

---
+++ Milestone_04 - Javascript/Module_19 - Function and Object/04_(advanced) Multiple parameter add, multiplication, etc/multi_perametter.js
@@ -0,0 +1,34 @@
+function add(n1, n2) {
+    var total = n1 + n2;
+    return total;
+}
+
+var sum = add(23, 45);
+console.log(sum);
+
+// Substructions
+function sub(n1, n2) {
+    var total = n1 - n2;
+    return total
+}
+
+var substraction = sub(93, 67);
+console.log(substraction);
+
+// Multipication
+function multi(n1, n2) {
+    var total = n1 * n2;
+    return total
+}
+
+var multipication = multi(5, 6);
+console.log(multipication);
+
+// Division
+function div(n1, n2) {
+    var total = n1 / n2;
+    return total;
+}
+
+var division = div(23, 4);
+console.log(division);
