0fa737b6cc30807bd2847e1176a09ccae40cfb38
Shama <assiddik001@gmail.com>
2022-01-30 14:41:30 +0600

    M20 V02 - Unit Convert

---
+++ Milestone_04 - Javascript/Module_20 - Apply JS Concepts/02_ Unit Convert Inch to Feet, miles to kilometer/unit_conveter.js
@@ -0,0 +1,15 @@
+// Inch to feet
+function inchToFeet(inches) {
+    var feet = inches / 12;
+    return feet;
+}
+
+console.log(inchToFeet(234));
+
+// Miles to Kilometer
+function milesToKilometer(miles) {
+    var km = miles * 1.60934;
+    return km;
+}
+
+console.log(milesToKilometer(23));
