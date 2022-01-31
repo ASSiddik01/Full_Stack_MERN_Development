7230885f77353719cc46ea694896026625d16a63
Shama <assiddik001@gmail.com>
2022-02-01 02:15:10 +0600

    M22 V01 - Math in JS

---
+++ Milestone_04 - Javascript/Module_21 - JS Simple Problem/01_Module Introduction, Math and Random number/math.js
@@ -0,0 +1,15 @@
+var value = 12.4;
+
+console.log(Math.abs(value));
+console.log(Math.round(value));
+console.log(Math.floor(value));
+console.log(Math.ceil(value));
+
+var randomValue = Math.random() * 10;
+console.log(Math.round(randomValue));
+
+for (let i = 0; i <= 20; i++) {
+    var randomValue = Math.random() * 10;
+    console.log(Math.round(randomValue));
+
+}
