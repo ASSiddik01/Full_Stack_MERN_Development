41cafadd6bef32453d5aad3a9e3d99dd3236e379
Shama <assiddik001@gmail.com>
2022-02-03 19:29:57 +0600

    M22.5 V01 - Basic Javascript concept checklist

---
+++ Milestone_04 - Javascript/Module_22.5 - JS Practices/01_Basic Javascript concept checklist and practice/concept_checklist.js
@@ -0,0 +1,17 @@
+// three variable decelar - string, number, boolean
+
+// use let and const
+
+// simple mart operartion
+
+// conparision <, >, == ,!=, <=, >=
+
+// condition && ||
+
+// if else
+
+// loop - while, for, for of
+
+// array
+
+// object
