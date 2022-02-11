901b06840dd1ba7e644c5bd7a7ceac2bc2fff798
Shama <assiddik001@gmail.com>
2022-02-11 18:43:43 +0600

    M27 V03 - Update deposit and balance

--- Milestone_05 - Integrate JS/Module_27 - bank by Function/banking.js
+++ Milestone_05 - Integrate JS/Module_27 - bank by Function/banking.js
@@ -1,16 +1,34 @@
 document.getElementById('deposite_button').addEventListener('click', function () {
-    // 1. Get Deposite value
-    const depositeInput = document.getElementById('deposite_amount');
-    const depositeAmount = depositeInput.value;
+    //1 Get Deposite amount
+    const depositeField = document.getElementById('deposite_amount');
+    const depositeText = depositeField.value;
+    // Empty error handle
+    if (depositeText == '') {
+        alert('Give a number');
+        return;
+    }
+    const depositeAmount = parseFloat(depositeText);
 
-    // 2. Get Previous Deposite
+    //2 Get Previous depositie
     const previousDepositeTotal = document.getElementById('deposite_total');
-    const previousDepositeAmount = (previousDepositeTotal.innerText);
+    const previousDepositeText = previousDepositeTotal.innerText;
+    const previousDepositeAmount = parseFloat(previousDepositeText);
 
-    // 3. Set Deposite amount
-    previousDepositeTotal.innerText = depositeAmount;
+    //3 Calculation Total Deposite
+    const totalDepositeAmount = previousDepositeAmount + depositeAmount;
 
-    // Clear
-    depositeInput.value = '';
+    //4 Set Total Deposite
+    previousDepositeTotal.innerText = totalDepositeAmount;
 
-})
+    //6.Get previous balace
+    const previousBalanceTotal = document.getElementById('balance_total');
+    const previousBalanceAmount = parseFloat(previousBalanceTotal.innerText);
+    
+    //7. Update Total Balance
+    previousBalanceTotal.innerText = previousBalanceAmount + depositeAmount;
+    
+
+    //5 Clear Deposite field
+    depositeField.value = '';
+
+});
