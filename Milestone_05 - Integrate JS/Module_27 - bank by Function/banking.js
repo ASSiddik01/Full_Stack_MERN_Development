3c6b31772f9a70baf5442ebd1b5e0598d7355645
Shama <assiddik001@gmail.com>
2022-02-11 21:33:43 +0600

    M27 V07 - Deposit and withdraw update by one function

--- Milestone_05 - Integrate JS/Module_27 - bank by Function/banking.js
+++ Milestone_05 - Integrate JS/Module_27 - bank by Function/banking.js
@@ -8,8 +8,18 @@ function getInputValue(inputId) {
     inputField.value = '';
 
     return inputAmount;
+}
 
+function updateTotalField(fieldId, Amount) {
+    const previousTotal = document.getElementById(fieldId);
+    const previousText = previousTotal.innerText;
+    const previousAmount = parseFloat(previousText);
 
+    //3 Calculation Total 
+    const totalAmount = previousAmount + Amount;
+
+    //4 Set Total 
+    previousTotal.innerText = totalAmount;
 }
 
 
@@ -19,16 +29,8 @@ document.getElementById('deposite_button').addEventListener('click', function ()
     //1 Get Deposite amount
     const depositeAmount = getInputValue('deposite_amount');
 
-    //2 Get Previous depositie
-    const previousDepositeTotal = document.getElementById('deposite_total');
-    const previousDepositeText = previousDepositeTotal.innerText;
-    const previousDepositeAmount = parseFloat(previousDepositeText);
-
-    //3 Calculation Total Deposite
-    const totalDepositeAmount = previousDepositeAmount + depositeAmount;
-
-    //4 Set Total Deposite
-    previousDepositeTotal.innerText = totalDepositeAmount;
+    //2 Update total
+    updateTotalField('deposite_total', depositeAmount);
 
     //6.Get previous balace
     const previousBalanceTotal = document.getElementById('balance_total');
@@ -48,18 +50,8 @@ document.getElementById('withdrow_button').addEventListener('click', function ()
     // 1. get Withdrow amount
     const withdrowAmount = getInputValue('withdraw_amount');
 
-
-
-    // 2. get previous withdown amount
-    const previousWithdrowTotal = document.getElementById('withdrow_total');
-    const previousWithdrowText = previousWithdrowTotal.innerText;
-    const previousWithdrowAmount = parseFloat(previousWithdrowText)
-
-    // 3. Calculation total Withdrow
-    const totalWithdrowAmount = previousWithdrowAmount + withdrowAmount;
-
-    // 4. Set Total withdrow
-    previousWithdrowTotal.innerText = totalWithdrowAmount;
+    // 2. Update total
+    updateTotalField('withdrow_total', withdrowAmount);
 
     //6.Get previous balace
     const previousBalanceTotal = document.getElementById('balance_total');
