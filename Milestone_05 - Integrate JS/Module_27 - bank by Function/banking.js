e2bd5eaf6028e5fa9cb03f36625982b63127095f
Shama <assiddik001@gmail.com>
2022-02-11 18:33:24 +0600

    M27 V02 - Get User deposit and set

--- Milestone_05 - Integrate JS/Module_27 - bank by Function/banking.js
+++ Milestone_05 - Integrate JS/Module_27 - bank by Function/banking.js
@@ -1,1 +1,16 @@
-console.log('Added JS');
+document.getElementById('deposite_button').addEventListener('click', function () {
+    // 1. Get Deposite value
+    const depositeInput = document.getElementById('deposite_amount');
+    const depositeAmount = depositeInput.value;
+
+    // 2. Get Previous Deposite
+    const previousDepositeTotal = document.getElementById('deposite_total');
+    const previousDepositeAmount = (previousDepositeTotal.innerText);
+
+    // 3. Set Deposite amount
+    previousDepositeTotal.innerText = depositeAmount;
+
+    // Clear
+    depositeInput.value = '';
+
+})
