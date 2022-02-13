b17051d84ddc07eb103dfda66c5a32c5445e6f3b
Shama <assiddik001@gmail.com>
2022-02-14 03:12:03 +0600

    M28 V04 - Update all Price

--- Milestone_05 - Integrate JS/Module_28 - Shopping Cart/app.js
+++ Milestone_05 - Integrate JS/Module_28 - Shopping Cart/app.js
@@ -1,4 +1,4 @@
-function updateCaseNumber(product, isIncrease, price) {
+function updateProduceNumber(product, isIncrease, price) {
     // 1.Get case input
     const productInput = document.getElementById(product + '_number');
 
@@ -15,24 +15,47 @@ function updateCaseNumber(product, isIncrease, price) {
     const casePrice = document.getElementById(product + '_price');
     casePrice.innerText = productNumber * price;
 
+    // Calculate total
+    calculateTotal();
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '_number');
+    let productNumber = parseInt(productInput.value);
+    return productNumber
+}
+
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    
+    // Calculation total price
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const total = subTotal + tax;
+
+    // Update total price
+    document.getElementById('sub_total').innerText = subTotal;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total_price').innerText = total;
 }
 
 // Phone handle
 document.getElementById('phone_plus').addEventListener('click', function () {
-    updateCaseNumber('phone', true, 1219);
+    updateProduceNumber('phone', true, 1219);
 });
 
 document.getElementById('phone_minus').addEventListener('click', function () {
-    updateCaseNumber('phone', false, 1219);
+    updateProduceNumber('phone', false, 1219);
 });
 
 
 // Case handle
 document.getElementById('case_plus').addEventListener('click', function () {
-    updateCaseNumber('case', true, 59);
+    updateProduceNumber('case', true, 59);
 });
 
 document.getElementById('case_minus').addEventListener('click', function () {
-    updateCaseNumber('case', false, 59);
+    updateProduceNumber('case', false, 59);
 });
 
