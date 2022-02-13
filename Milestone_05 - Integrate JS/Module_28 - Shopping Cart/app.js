199d98449e3890a0c6109180d7343800348da258
Shama <assiddik001@gmail.com>
2022-02-13 19:05:46 +0600

    M28 V02 - Decrease and update price

--- Milestone_05 - Integrate JS/Module_28 - Shopping Cart/app.js
+++ Milestone_05 - Integrate JS/Module_28 - Shopping Cart/app.js
@@ -1,8 +1,26 @@
-document.getElementById('case_plus').addEventListener('click', function () {
+function updateCaseNumber(isIncrease) {
     // 1.Get case input
     const caseInput = document.getElementById('case_number');
-    const caseCount = caseInput.value;
+    let caseNumber = caseInput.value;
     // 2.Set case value
-    caseInput.value = parseInt(caseCount) + 1;
-    // console.log(caseCount);
-})
+    if (isIncrease == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    } else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+
+    // update case price
+    const casePrice = document.getElementById('case_price');
+    casePrice.innerText = caseNumber * 59;
+    
+}
+
+document.getElementById('case_plus').addEventListener('click', function () {
+    updateCaseNumber(true);
+});
+
+document.getElementById('case_minus').addEventListener('click', function () {
+    updateCaseNumber(false)
+});
+
