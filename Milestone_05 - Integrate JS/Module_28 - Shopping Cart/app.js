e65fdac8a64f80135c989264dfa0fdefcff6eaab
Shama <assiddik001@gmail.com>
2022-02-13 19:28:29 +0600

    M28 V03 - Same shared function

--- Milestone_05 - Integrate JS/Module_28 - Shopping Cart/app.js
+++ Milestone_05 - Integrate JS/Module_28 - Shopping Cart/app.js
@@ -1,26 +1,38 @@
-function updateCaseNumber(isIncrease) {
+function updateCaseNumber(product, isIncrease, price) {
     // 1.Get case input
-    const caseInput = document.getElementById('case_number');
-    let caseNumber = caseInput.value;
+    const productInput = document.getElementById(product + '_number');
+
+    let productNumber = productInput.value;
     // 2.Set case value
     if (isIncrease == true) {
-        caseNumber = parseInt(caseNumber) + 1;
-    } else if (caseNumber > 0) {
-        caseNumber = parseInt(caseNumber) - 1;
+        productNumber = parseInt(productNumber) + 1;
+    } else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
+    productInput.value = productNumber;
 
     // update case price
-    const casePrice = document.getElementById('case_price');
-    casePrice.innerText = caseNumber * 59;
-    
+    const casePrice = document.getElementById(product + '_price');
+    casePrice.innerText = productNumber * price;
+
 }
 
+// Phone handle
+document.getElementById('phone_plus').addEventListener('click', function () {
+    updateCaseNumber('phone', true, 1219);
+});
+
+document.getElementById('phone_minus').addEventListener('click', function () {
+    updateCaseNumber('phone', false, 1219);
+});
+
+
+// Case handle
 document.getElementById('case_plus').addEventListener('click', function () {
-    updateCaseNumber(true);
+    updateCaseNumber('case', true, 59);
 });
 
 document.getElementById('case_minus').addEventListener('click', function () {
-    updateCaseNumber(false)
+    updateCaseNumber('case', false, 59);
 });
 
