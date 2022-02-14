9a4d2d7b92078d655554c7c622258ff499d08a2a
Shama <assiddik001@gmail.com>
2022-02-14 17:35:40 +0600

    M28 V06 - Create calculator and clear

--- Milestone_05 - Integrate JS/Module_28 - Shopping Cart/pin-matcher/js/app.js
+++ Milestone_05 - Integrate JS/Module_28 - Shopping Cart/pin-matcher/js/app.js
@@ -13,3 +13,20 @@ function generatePin() {
     const pin = getPin()
     document.getElementById('pin_show').value = pin;
 }
+
+
+// Key pad
+document.getElementById('key_pad').addEventListener('click', function (event) { 
+    const number = event.target.innerText;
+    const givePin = document.getElementById('pin_input');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            givePin.value = '';
+        }
+    } else {
+        const previousPin = givePin.value;
+        const newPin = previousPin + number;
+        givePin.value = newPin; 
+    }
+    
+})
