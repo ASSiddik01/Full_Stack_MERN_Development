6b2f7e773bc9cf569f2815e3b35fc75f64f9a0c3
Shama <assiddik001@gmail.com>
2022-02-15 16:01:12 +0600

    M29 V08 - Explore Try-catch

---
+++ Milestone_05 - Integrate JS/Module_29 - String and Array/08_try_catch.js
@@ -0,0 +1,20 @@
+const myName = 'Abu Shama';
+
+
+try {
+    myName = 'Siddik';
+} catch (error) {
+    
+}
+
+
+
+// Type error
+// myName = 'Siddik';
+
+// Syntex error
+// for (i = 1; i < 10;i++){
+//     console.log(i);
+// }
+
+console.log(myName);
