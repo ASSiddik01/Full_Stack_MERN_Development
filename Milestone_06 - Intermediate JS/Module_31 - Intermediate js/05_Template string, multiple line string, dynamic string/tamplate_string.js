ad1bd98b51b4ec36016d189948efe88596d2aa39
Shama <assiddik001@gmail.com>
2022-02-21 17:25:01 +0600

    M31 V05 - Template string

---
+++ Milestone_06 - Intermediate JS/Module_31 - Intermediate js/05_Template string, multiple line string, dynamic string/tamplate_string.js
@@ -0,0 +1,19 @@
+// var string = "Amar sonar bangla\n"+
+// "Ami tomai valobai\n";
+
+// Multiline
+var string = `Amar sonar bangla
+ami tomai valobasi`;
+
+console.log(string);
+
+
+// Dynamic string
+let firstName = "Abu";
+let lastName = "Shama";
+
+// const fullName = firstName + ' ' + lastName;
+
+const myName = `My name is: ${firstName} ${lastName}.`
+
+console.log(myName);
