5018838c55effa7a5378f94e2e692c27da9015b6
Shama <assiddik001@gmail.com>
2022-02-21 17:47:44 +0600

    M31 V07 - Big arrow function

---
+++ Milestone_06 - Intermediate JS/Module_31 - Intermediate js/07_More Arrow functions and big arrow function/big_arrow.js
@@ -0,0 +1,18 @@
+const add = (num1, num2) => num1 + num2;
+console.log(add(2, 3));
+
+const multiply = num => num * 2;
+console.log(multiply(2));
+
+const myName = () => 'Abu Shama';
+console.log(myName());
+
+
+const doMath = (x, y) => {
+    const sum = x + y;
+    const sub = x - y;
+    const result = sum * sub;
+    return result;
+}
+
+console.log(doMath(7, 5));
