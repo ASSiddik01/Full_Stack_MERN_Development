5dedc227ab285870a599de67c78bf79165151e30
Shama <assiddik001@gmail.com>
2022-02-22 16:40:41 +0600

    M33 V02 - JSON parse, stringify

--- Milestone_06 - Intermediate JS/Module_33 - API, JSON, Dynamic site/index.js
+++ Milestone_06 - Intermediate JS/Module_33 - API, JSON, Dynamic site/index.js
@@ -1,1 +1,18 @@
-console.log('API');
+// Step 1
+const shop = {
+    name: "Siddik Store",
+    address: "Chuadanga",
+    profit: 50000,
+    products: ['laptop', 'mobile', 'gadgets'],
+    owner : {
+        name: "Shama",
+        positon: "head"
+    },
+    isExpensive : true
+}
+
+const stringified = JSON.stringify(shop);
+console.log(stringified);
+
+const parsed = JSON.parse(stringified);
+console.log(parsed);
