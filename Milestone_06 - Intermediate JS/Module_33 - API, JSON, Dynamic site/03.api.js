6215ec7bf1d01c75119c0c5e4004bee5413afd3d
Shama <assiddik001@gmail.com>
2022-02-22 17:34:02 +0600

    M33 V04 - Load more data

--- Milestone_06 - Intermediate JS/Module_33 - API, JSON, Dynamic site/03.api.js
+++ Milestone_06 - Intermediate JS/Module_33 - API, JSON, Dynamic site/03.api.js
@@ -10,3 +10,29 @@ function loadData() {
         .then(json => console.log(json))
 
 }
+
+// Load Users
+
+// Step 4
+function loadUsers() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => console.log(users))
+
+}
+
+// Load Post
+
+// Step 4
+function loadPosts() {
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(posts => displayPost(posts))
+
+}
+
+function displayPost(posts) {
+    console.log(posts);
+}
