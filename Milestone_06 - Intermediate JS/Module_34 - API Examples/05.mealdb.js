08f442be4be0caa16916194ac7b7ccf7a59fcff4
Shama <assiddik001@gmail.com>
2022-02-25 17:53:59 +0600

    M34 V07 - Create dynamic url based on click

--- Milestone_06 - Intermediate JS/Module_34 - API Examples/05.mealdb.js
+++ Milestone_06 - Intermediate JS/Module_34 - API Examples/05.mealdb.js
@@ -22,19 +22,51 @@ const displayFood = meals => {
   // Step 7
   searchResult.innerHTML = '';
   // step 6
-    meals.forEach(meal => {
+  meals.forEach(meal => {
     // console.log(meal);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+                <!-- ...........Step 8........... -->
           <div onclick="loadFoodDetail(${meal.idMeal})" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${meal.strMeal}</h5>
-              <p class="card-text">${meal.strInstructions.slice(0,200)}.</p>
+              <p class="card-text">${meal.strInstructions.slice(0, 200)}.</p>
             </div>
           </div>
         `;
-        searchResult.appendChild(div);
-    })
+    searchResult.appendChild(div);
+  })
+}
+
+// Step 7
+const loadFoodDetail = mealId => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+  fetch(url)
+    .then(res => res.json())
+    // .then(data => console.log(data.meals[0]));
+    // Step 10
+    .then(data => displayMealDetaile(data.meals[0]));
+}
+
+// Step 9
+const displayMealDetaile = meal => {
+  // console.log(meal);
+  const mealDetailes = document.getElementById('meal_detiles');
+  // console.log(mealDetailes);
+  // Step 12
+  mealDetailes.textContent = '';
+  // Step 11
+  const div = document.createElement('div');
+  div.classList.add('card')
+  div.innerHTML = `
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${meal.strMeal}</h5>
+    <p class="card-text">${meal.strInstructions.slice(0, 150)}.</p>
+    <a href="${meal.strYoutube}" class="btn btn-primary">Youtube links</a>
+  </div>
+  `;
+  mealDetailes.appendChild(div);
 }
