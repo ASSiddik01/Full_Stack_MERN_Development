28a4a83a1f29162d5b4fe348447e80b9abc620d3
Shama <assiddik001@gmail.com>
2022-02-25 17:39:17 +0600

    M34 V06 - Display dynamic search result

--- Milestone_06 - Intermediate JS/Module_34 - API Examples/05.mealdb.js
+++ Milestone_06 - Intermediate JS/Module_34 - API Examples/05.mealdb.js
@@ -10,5 +10,31 @@ const searchFood = () => {
   // Step 3
   fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.meals));
+    // .then(data => console.log(data.meals));
+    // Step 5
+    .then(data => displayFood(data.meals));
+}
+
+// Step 4
+const displayFood = meals => {
+  // console.log(meals)
+  const searchResult = document.getElementById('search_result');
+  // Step 7
+  searchResult.innerHTML = '';
+  // step 6
+    meals.forEach(meal => {
+    // console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+          <div onclick="loadFoodDetail(${meal.idMeal})" class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">${meal.strInstructions.slice(0,200)}.</p>
+            </div>
+          </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
