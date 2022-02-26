3fbc8d848f7f43dc1f986378a5202bc862eaf325
Shama <assiddik001@gmail.com>
2022-02-26 15:00:59 +0600

    M35 V03 - Truthy and Falsy values

---
+++ Milestone_06 - Intermediate JS/Module_35 - JS Tricky Concepts/03.true_false.js
@@ -0,0 +1,27 @@
+/*
+False Condition:
+false
+0
+empty string
+undefind
+null
+NaN
+
+
+True Condition:
+true
+any negative or positive number
+string include white space string
+empty array
+empty object
+*/
+
+
+
+let x ={};
+console.log(x)
+if (x) {
+    console.log("variable is true");
+} else {
+    console.log("variable is false");
+}
