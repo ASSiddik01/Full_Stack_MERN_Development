561a0383f9ae3cf402d7cd27376f5eb4710823b8
Shama <assiddik001@gmail.com>
2022-02-26 15:28:24 +0600

    M35 V06 - Scope

---
+++ Milestone_06 - Intermediate JS/Module_35 - JS Tricky Concepts/06.scope.js
@@ -0,0 +1,11 @@
+let name = 'Abu shama'; //global scope
+
+function sum(x, y) {
+    return x + y;
+}
+// console.log(sum(x)) // local scope
+
+for (var i = 0; i < 10; i++) { //Block Scope
+
+}
+console.log(i);
