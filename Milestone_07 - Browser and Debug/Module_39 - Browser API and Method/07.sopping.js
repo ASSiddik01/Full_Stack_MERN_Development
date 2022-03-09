5b310358e990de8b004c4d5a03652f50feebcfd6
Shama <assiddik001@gmail.com>
2022-03-09 14:36:24 +0600

    M39 V08 - Retrieve local storage value and display

--- Milestone_07 - Browser and Debug/Module_39 - Browser API and Method/07.sopping.js
+++ Milestone_07 - Browser and Debug/Module_39 - Browser API and Method/07.sopping.js
@@ -3,6 +3,10 @@ const addItem = () => {
     const nameField = document.getElementById('product_name');
     const name = nameField.value;
     // console.log(name)
+    // step 8 empty not add to cart
+    if (!name) {
+        return;
+    }
     // step 3 call display product function
     displayProduct(name);
     // step 6 call add to cart
@@ -35,8 +39,32 @@ const getCart = () => {
 // step 5 add product to cart
 const addToCart = name => {
     const cart = getCart();
-    cart[name] = 1;
+    // cart[name] = 1;
     // console.log(cart);
+    // step 10 add existing product 
+    if (cart[name]) {
+        cart[name] += 1;
+    } else {
+        cart[name] = 1
+    }
+    
     const cartStringfied = JSON.stringify(cart);
     localStorage.setItem('cart',cartStringfied);
+}
+
+// step 7 load local storage product
+const displayLocalStorageCart = () => {
+    const cart = getCart();
+    // console.log(cart);
+    for (const product in cart) {
+        // console.log(product);
+        displayProduct(product);
+    }
+}
+displayLocalStorageCart();
+
+// step 9 place order
+const placeOrder = () => {
+    document.getElementById('products').textContent = '';
+    localStorage.removeItem('cart');
 }
