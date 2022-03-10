204b1497a5714ccfaec83736b33781c44e143f45
Shama <assiddik001@gmail.com>
2022-03-10 20:35:04 +0600

    M41 V06 - Fix Duplicate names

--- Milestone_07 - Browser and Debug/Module_41 - Few More JS Debug/salary-app/salaryApp.js
+++ Milestone_07 - Browser and Debug/Module_41 - Few More JS Debug/salary-app/salaryApp.js
@@ -227,7 +227,9 @@ const uniquifyNames = function (items) {
   const uniqueNames = {};
 
   return items.map(function (item) {
-    if (uniqueNames[item.name]) {
+    // if (uniqueNames[item.name]) {
+    // Problem 2 
+    if (uniqueNames[item.name] !== undefined) {
       uniqueNames[item.name] += " ";
       item.name += uniqueNames[item.name];
     } else {
