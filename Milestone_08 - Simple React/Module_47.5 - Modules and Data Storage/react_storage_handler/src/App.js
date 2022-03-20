53c50ea14d755e5f3a33bb243df9a00a3c802053
Shama <assiddik001@gmail.com>
2022-03-21 00:04:41 +0600

    M47.5 V01 - Simple import, export

---
+++ Milestone_08 - Simple React/Module_47.5 - Modules and Data Storage/react_storage_handler/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Cosmatic from './components/Cosmatic/Cosmatic';
+
+function App() {
+  return (
+    <div className="App">
+      <Cosmatic></Cosmatic>
+    </div>
+  );
+}
+
+export default App;
---
+++ Milestone_08 - Simple React/Module_47.5 - Modules and Data Storage/react_storage_handler/src/components/Cosmatic/Cosmatic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmatic = () => {
+    return (
+        <div>
+            <h2>This is Cosmatic</h2>
+        </div>
+    );
+};
+
+export default Cosmatic;
