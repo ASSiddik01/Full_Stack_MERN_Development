a0e591d4988eb247697b902e37aeb9b1843642d0
Shama <assiddik001@gmail.com>
2022-03-21 17:53:08 +0600

    M47.5 V06 - Shopping Cart

--- Milestone_08 - Simple React/Module_47.5 - Modules and Data Storage/react_storage_handler/src/uitilities/fakedb.js
+++ Milestone_08 - Simple React/Module_47.5 - Modules and Data Storage/react_storage_handler/src/uitilities/fakedb.js
@@ -1,14 +1,36 @@
 const addToDb = (id) => {
-  const quentity = localStorage.getItem(id);
-  if (quentity) {
-      console.log('Old Item')
-      const newQuentity = +quentity + 1;
-    localStorage.setItem(id, newQuentity);
+  let shoppingCart;
 
+  // Get Quentity
+  const storedCart = localStorage.getItem("cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
   } else {
-      console.log('New Item')
-    localStorage.setItem(id, 1);
+    shoppingCart = {};
   }
+    
+  // Add Quentity
+  const quentity = shoppingCart[id];
+  if (quentity) {
+    const newQuentity = + quentity + 1;
+    shoppingCart[id] = newQuentity;
+  } else {
+    shoppingCart[id] = 1;
+  }
+  localStorage.setItem("cart", JSON.stringify(shoppingCart));
 };
 
 export { addToDb };
+
+// const addToDb = (id) => {
+//   const quentity = localStorage.getItem(id);
+//   if (quentity) {
+//       console.log('Old Item')
+//       const newQuentity = +quentity + 1;
+//     localStorage.setItem(id, newQuentity);
+
+//   } else {
+//       console.log('New Item')
+//     localStorage.setItem(id, 1);
+//   }
+// };
