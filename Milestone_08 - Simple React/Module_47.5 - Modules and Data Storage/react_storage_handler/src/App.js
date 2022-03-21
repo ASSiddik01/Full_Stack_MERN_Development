4351a2e4dbc3ba5a50a37ddc07de9ccc7795d316
Shama <assiddik001@gmail.com>
2022-03-21 09:49:59 +0600

    M47.5 V02 - Multiple import, export, relative path

--- Milestone_08 - Simple React/Module_47.5 - Modules and Data Storage/react_storage_handler/src/App.js
+++ Milestone_08 - Simple React/Module_47.5 - Modules and Data Storage/react_storage_handler/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Cosmatic from './components/Cosmatic/Cosmatic';
+import Shoes from './components/Shoes/Shoes';
 
 function App() {
   return (
     <div className="App">
       <Cosmatic></Cosmatic>
+      <Shoes></Shoes>
     </div>
   );
 }
---
+++ Milestone_08 - Simple React/Module_47.5 - Modules and Data Storage/react_storage_handler/src/utilities/calculate/calculate.js
@@ -0,0 +1,11 @@
+const add = (n1, n2) => {
+    return n1 + n2;
+}
+
+// export default add
+
+const multiply = (n1, n2) => {
+    return n1 * n2;
+}
+
+export {add, multiply}
