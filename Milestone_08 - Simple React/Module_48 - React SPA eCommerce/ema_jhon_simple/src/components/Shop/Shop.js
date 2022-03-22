7fff9fb0df1eb00e4150e2ed5a55977769bed8d3
Shama <assiddik001@gmail.com>
2022-03-22 12:41:08 +0600

    M48 V07 - Cart state setup

--- Milestone_08 - Simple React/Module_48 - React SPA eCommerce/ema_jhon_simple/src/components/Shop/Shop.js
+++ Milestone_08 - Simple React/Module_48 - React SPA eCommerce/ema_jhon_simple/src/components/Shop/Shop.js
@@ -1,40 +1,45 @@
-import React from 'react';
-import './Shop.css'
-import { useState } from 'react';
-import { useEffect } from 'react';
-import Product from '../Product/Product';
-
+import React from "react";
+import "./Shop.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import Product from "../Product/Product";
 
 const Shop = () => {
-    // Step 3
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data));
-    }, [])
+  // Step 3
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    // Step 6
-    const handleAddToCart = (product) => {
-        console.log(product)
-    }
-    return (
-        <div className='shop_container'>
-            <div className="product_container">
-                {/* Setp 4 */}
-                {
-                    products.map(product => <Product
-                        key={product.id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
-            </div>
-            <div className="cart_container">
-                <h3>Order Summary</h3>
-            </div>
-        </div>
-    );
+  // Step 7
+  const [cart, setCart] = useState([]);
+
+  // Step 6
+  const handleAddToCart = (product) => {
+    // console.log(product)
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+  return (
+    <div className="shop_container">
+      <div className="product_container">
+        {/* Setp 4 */}
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart_container">
+              <h3>Order Summary</h3>
+              <p>Selected Items: {cart.length} </p>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
