074c29900221ace83e8188497e816947202f96e0
Shama <assiddik001@gmail.com>
2022-03-29 14:41:17 +0600

    M50.5 V02 - Stored in local storage

--- Milestone_08 - Simple React/Module_50.5 - React Bonus/local_baba/js/app.js
+++ Milestone_08 - Simple React/Module_50.5 - React Bonus/local_baba/js/app.js
@@ -1,7 +1,15 @@
 const db = {};
 
 const addToDb = item => {
-    // Step 1
-    db[item] = 1;
+    // Step 1           
+    // db[item] = 1;
+
+    if (item in db) {
+        db[item] = db[item] + 1;
+    } else {
+        db[item] = 1;
+    }
+
     console.log(db);
+
 }
