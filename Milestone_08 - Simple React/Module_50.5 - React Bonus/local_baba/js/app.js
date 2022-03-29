f45acf253e13ca6697a8abed375c0b1f4c30316d
Shama <assiddik001@gmail.com>
2022-03-29 15:02:55 +0600

    M50.5 V03 - Parsist and remove

--- Milestone_08 - Simple React/Module_50.5 - React Bonus/local_baba/js/app.js
+++ Milestone_08 - Simple React/Module_50.5 - React Bonus/local_baba/js/app.js
@@ -1,15 +1,32 @@
-const db = {};
+let db = {};
 
-const addToDb = item => {
-    // Step 1           
-    // db[item] = 1;
+const addToDb = (item) => {
+  // Step 1
+  // db[item] = 1;
 
-    if (item in db) {
-        db[item] = db[item] + 1;
-    } else {
-        db[item] = 1;
-    }
+  // Step 4
+  const storedUser = localStorage.getItem("users");
+  if (storedUser) {
+    db = JSON.parse(storedUser);
+  }
 
-    console.log(db);
+  // Step 2
+  if (item in db) {
+    db[item] = db[item] + 1;
+  } else {
+    db[item] = 1;
+  }
 
-}
+  // Step 3
+  localStorage.setItem("users", JSON.stringify(db));
+};
+
+// Step 5
+const removeFromDb = (item) => {
+  const storedUser = localStorage.getItem("users");
+  if (storedUser) {
+    const previousUser = JSON.parse(storedUser);
+    delete previousUser[item];
+    localStorage.setItem("users", JSON.stringify(previousUser));
+  }
+};
