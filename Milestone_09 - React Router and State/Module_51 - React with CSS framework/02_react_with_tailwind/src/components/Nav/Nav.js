02ee767829de3d89eb7a73306e9db805fa6f8bef
Shama <assiddik001@gmail.com>
2022-04-03 04:29:44 +0600

    M51 V04 - Setup heroicon

--- Milestone_09 - React Router and State/Module_51 - React with CSS framework/02_react_with_tailwind/src/components/Nav/Nav.js
+++ Milestone_09 - React Router and State/Module_51 - React with CSS framework/02_react_with_tailwind/src/components/Nav/Nav.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import Link from '../Link/Link';
+import React from "react";
+import Link from "../Link/Link";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
 const Nav = () => {
-    const routes = [
-        {id:1, name:'Home', link: '/home'},
-        {id:2, name:'Shop', link: '/shop'},
-        {id:3, name:'Deals', link: '/deals'},
-        {id:4, name:'Coupons', link: '/coupons'},
-        {id:5, name:'Contact Us', link: '/contact'},
-    ]
-    return (
-        <nav>
-            <ul>
-                {
-                    routes.map(route => <Link
-                        key={route.id}
-                        route={route}
-                    ></Link>)
-            }
-            </ul>
-        </nav>
-    );
+  const [open, setOpen] = useState(false);
+  const routes = [
+    { id: 1, name: "Home", link: "/home" },
+    { id: 2, name: "Shop", link: "/shop" },
+    { id: 3, name: "Deals", link: "/deals" },
+    { id: 4, name: "Coupons", link: "/coupons" },
+    { id: 5, name: "Contact Us", link: "/contact" },
+  ];
+  return (
+    <nav>
+      <div onClick={() => setOpen(!open)} className="w-6 h-6 md:hidden">
+        {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+      </div>
+      <ul
+        className={`md:flex justify-center absolute duration-500 ease-in ${
+          open ? "top-6" : "top-[-120px]"
+        } `}
+      >
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
