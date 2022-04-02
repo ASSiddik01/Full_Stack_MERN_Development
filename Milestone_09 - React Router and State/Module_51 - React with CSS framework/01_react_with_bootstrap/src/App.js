995c754a8f5196cfe8cc8b2f3013ccc6e96a5479
Shama <assiddik001@gmail.com>
2022-04-02 16:33:11 +0600

    M51 V02 - Install React Bootstrap

--- Milestone_09 - React Router and State/Module_51 - React with CSS framework/01_react_with_bootstrap/src/App.js
+++ Milestone_09 - React Router and State/Module_51 - React with CSS framework/01_react_with_bootstrap/src/App.js
@@ -1,37 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Button, Spinner } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <button type="button" className="btn btn-primary">
+      {/* From CDN */}
+      {/* <button type="button" className="btn btn-primary">
         Primary
-      </button>
-      <button type="button" className="btn btn-secondary">
-        Secondary
-      </button>
-      <button type="button" className="btn btn-success">
-        Success
-      </button>
-      <button type="button" className="btn btn-danger">
-        Danger
-      </button>
-      <button type="button" className="btn btn-warning">
-        Warning
-      </button>
-      <button type="button" className="btn btn-info">
-        Info
-      </button>
-      <button type="button" className="btn btn-light">
-        Light
-      </button>
-      <button type="button" className="btn btn-dark">
-        Dark
-      </button>
+      </button> */}
+      {/*  */}
 
-      <button type="button" className="btn btn-link">
-        Link
-      </button>
+      {/* React Bootstrap */}
+      <Button variant="primary">Primary</Button>
+      <br />
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
     </div>
   );
 }
