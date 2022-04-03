63076ca6e5d03a8a689e2a7834d55769aa556450
Shama <assiddik001@gmail.com>
2022-04-03 10:42:47 +0600

    M52 V02 - Create Simple Route

--- Milestone_09 - React Router and State/Module_52 - Simple React Router/01_simple_react_route/src/App.js
+++ Milestone_09 - React Router and State/Module_52 - Simple React Router/01_simple_react_route/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Home from './components/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Friend from './components/Friend/Friend';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/friends" element={<Friend />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
