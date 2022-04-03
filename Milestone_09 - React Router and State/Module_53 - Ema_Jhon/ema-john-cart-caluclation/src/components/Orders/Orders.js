3633c725ed588714c71f150f038ef1bb8fc54b30
Shama <assiddik001@gmail.com>
2022-04-03 17:58:38 +0600

    M53 V03 - Create hooks and export and import it

--- Milestone_09 - React Router and State/Module_53 - Ema_Jhon/ema-john-cart-caluclation/src/components/Orders/Orders.js
+++ Milestone_09 - React Router and State/Module_53 - Ema_Jhon/ema-john-cart-caluclation/src/components/Orders/Orders.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 import './Orders.css'
 
 const Orders = () => {
+    const [products, setProducts] = useProducts();
     return (
         <div>
-            <h2>Orders</h2>
+            <h2>Orders {products.length} </h2>
         </div>
     );
 };
