d5c40bd4152d35b7d378912b7be8e495d942586c
Shama <assiddik001@gmail.com>
2022-04-03 10:11:44 +0600

    M51 V09 - React spring animation

--- Milestone_09 - React Router and State/Module_51 - React with CSS framework/03_react_rechart/src/App.js
+++ Milestone_09 - React Router and State/Module_51 - React with CSS framework/03_react_rechart/src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import Chart from './components/Chart/Chart';
-import SpecialChart from './components/SpecialChart/SpecialChart';
+import "./App.css";
+import Chart from "./components/Chart/Chart";
+import SpecialChart from "./components/SpecialChart/SpecialChart";
+import { useSpring, animated } from 'react-spring'
+
+// function App() {
+//   const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
+//   return (
+//     <div className="App">
+//       <animated.div style={props}>I will fade in</animated.div>
+//       <Chart></Chart>
+//       <SpecialChart></SpecialChart>
+//     </div>
+//   );
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <Chart></Chart>
-      <SpecialChart></SpecialChart>
-    </div>
-  );
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
+  return <animated.div style={props}>I will fade in</animated.div>
 }
 
 export default App;
