c16d0735455e1a939807e2f9d8bd07b5c67e71ec
Shama <assiddik001@gmail.com>
2022-04-04 04:07:50 +0600

    M53 V06 - Implement review item

--- Milestone_09 - React Router and State/Module_53 - Ema_Jhon/ema-john-cart-caluclation/src/components/ReviewItem/ReviewItem.js
+++ Milestone_09 - React Router and State/Module_53 - Ema_Jhon/ema-john-cart-caluclation/src/components/ReviewItem/ReviewItem.js
@@ -2,10 +2,33 @@ import React from "react";
 import "./ReviewItem.css";
 
 const ReviewItem = (props) => {
-  const { name, price, shipping, quantity } = props.product;
+  const { name, img, price, shipping, quantity } = props.product;
   return (
-    <div>
-      <h2>Review</h2>
+    <div className="review-item">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div className="review-item-details-container">
+        <div className="review-item-details">
+          <p className="product-name" title={name}>
+            {" "}
+            {name.length > 20 ? name.slice(0, 20) + "...": name}{" "}
+          </p>
+          <p className="price">
+            <small>
+              Price: <span> {price} </span>{" "}
+            </small>
+          </p>
+          <p className="shipping">
+            <small>
+              Shipping: <span> {shipping} </span>{" "}
+            </small>
+          </p>
+        </div>
+        <div className="delete-container">
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 };
