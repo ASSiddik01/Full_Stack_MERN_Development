6680348499f589fd00e22e09c446ff6a5697f6a1
Shama <assiddik001@gmail.com>
2022-04-04 04:44:41 +0600

    M53 V09 - Module summmary

--- Milestone_09 - React Router and State/Module_53 - Ema_Jhon/ema-john-cart-caluclation/src/components/Orders/Orders.js
+++ Milestone_09 - React Router and State/Module_53 - Ema_Jhon/ema-john-cart-caluclation/src/components/Orders/Orders.js
@@ -6,31 +6,34 @@ import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import ReviewItem from "../ReviewItem/ReviewItem";
 import { removeFromDb } from "../../utilities/fakedb";
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Orders = () => {
   const [products, setProducts] = useProducts();
   const [cart, setCart] = useCart(products);
 
-  const handleRemoveProduct = product => {
-    const rest = cart.filter(pd => pd.id !== product.id);
+  const handleRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd.id !== product.id);
     setCart(rest);
     removeFromDb(product.id);
-  }
+  };
+
+  let navigate = useNavigate();
 
   return (
     <div className="shop-container">
       <div className="review-items-container">
         {cart.map((product) => (
-          <ReviewItem key={product.id} product={product} handleRemoveProduct={handleRemoveProduct} ></ReviewItem>
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveProduct={handleRemoveProduct}
+          ></ReviewItem>
         ))}
       </div>
       <div className="cart-container">
         <Cart cart={cart}>
-
-        <Link to='/orders'>
-                    <button>Proceed Checkout</button>
-                    </Link>
+          <button onClick={()=>navigate('/shop')}>Proceed Checkout</button>
         </Cart>
       </div>
     </div>
