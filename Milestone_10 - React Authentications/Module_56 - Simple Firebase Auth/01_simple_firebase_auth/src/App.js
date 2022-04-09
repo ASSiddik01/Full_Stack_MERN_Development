00634a58d6daafd47bad2e3c589a795046aa33cc
Shama <assiddik001@gmail.com>
2022-04-09 11:14:46 +0600

    M56 V06 - Display logged in user info

--- Milestone_10 - React Authentications/Module_56 - Simple Firebase Auth/01_simple_firebase_auth/src/App.js
+++ Milestone_10 - React Authentications/Module_56 - Simple Firebase Auth/01_simple_firebase_auth/src/App.js
@@ -1,24 +1,48 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 
 function App() {
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {user.email ? (
+        <button onClick={handleGoogleSignOut}>Google Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      )}
+      <h2>User: {user.displayName} </h2>
     </div>
   );
 }
