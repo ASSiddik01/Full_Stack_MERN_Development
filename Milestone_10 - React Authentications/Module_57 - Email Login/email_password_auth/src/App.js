25d5f62375b115abd4a1ba691728d48ac8190e1c
Shama <assiddik001@gmail.com>
2022-04-11 03:36:02 +0600

    M57 V02 - Event discussion

--- Milestone_10 - React Authentications/Module_57 - Email Login/email_password_auth/src/App.js
+++ Milestone_10 - React Authentications/Module_57 - Email Login/email_password_auth/src/App.js
@@ -8,9 +8,27 @@ import { getAuth } from "firebase/auth";
 const auth = getAuth(app);
 
 function App() {
+
+  const handleEmailInput = (e) => {
+    console.log(e.target.value)
+  }
+  const handlePasswordInput = (e) => {
+    console.log(e.target.value)
+  }
+  const handleFromSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  
   return (
     <div className="App">
-      
+      <form onSubmit={ handleFromSubmit}>
+        <input onBlur={handleEmailInput} type="text" name="" id="" />
+        <br />
+        <input onBlur={handlePasswordInput} type="password" name="" id="" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     
     </div>
   );
