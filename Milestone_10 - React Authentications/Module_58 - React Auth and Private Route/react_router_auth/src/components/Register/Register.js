3558f10f6bbde943af780d00622dcdd927e19f8f
Shama <assiddik001@gmail.com>
2022-04-11 16:50:14 +0600

    M58 V02 - Create use firebase hook

--- Milestone_10 - React Authentications/Module_58 - React Auth and Private Route/react_router_auth/src/components/Register/Register.js
+++ Milestone_10 - React Authentications/Module_58 - React Auth and Private Route/react_router_auth/src/components/Register/Register.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import './Login.css'
-const Login = () => {
+import './Register.css'
+
+const Register = () => {
     return (
         <div>
-            <h3>Please Login</h3>
+            <h3>Please Register</h3>
             <form action="">
+                <input type="text" name="" id="" placeholder='Your name' />
+                <br />
                 <input type="email" name="" id="" placeholder='Your email' />
                 <br />
                 <input type="password" name="" id="" placeholder='Your password' />
                 <br />
-                <input type="submit" value="Login" />
+                <input type="submit" value="Register" />
             </form>
         </div>
     );
 };
 
-export default Login;
+export default Register;
