4710e1f6dba4ca973715437dac618592f7d31ef1
Shama <assiddik001@gmail.com>
2022-04-11 16:33:52 +0600

    M58 V01 - Project Setup

---
+++ Milestone_10 - React Authentications/Module_58 - React Auth and Private Route/react_router_auth/src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpyt0G6g46bMY4oL3sOiQgMY-ysPBYazM",
+  authDomain: "react-router-auth-a01f1.firebaseapp.com",
+  projectId: "react-router-auth-a01f1",
+  storageBucket: "react-router-auth-a01f1.appspot.com",
+  messagingSenderId: "448204453342",
+  appId: "1:448204453342:web:13ac2b3c9d045d7e5fd94f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
