05e87abdafbd0c4cf45b73333ca70175b09c8f85
Shama <assiddik001@gmail.com>
2022-04-12 11:12:09 +0600

    M58 V05 - React firebase hooks

--- Milestone_10 - React Authentications/Module_58 - React Auth and Private Route/react_router_auth/src/components/Header/Header.js
+++ Milestone_10 - React Authentications/Module_58 - React Auth and Private Route/react_router_auth/src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     <div className="header">
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/product">Products</Link>
+        <Link to="/products">Products</Link>
         <Link to="/order">Orders</Link>
         <Link to="/review">Reviews</Link>
         <Link to="/register">Register</Link>
--- Milestone_10 - React Authentications/Module_58 - React Auth and Private Route/react_router_auth/src/components/Home/Home.js
+++ Milestone_10 - React Authentications/Module_58 - React Auth and Private Route/react_router_auth/src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Home = () => {
+    const { user } = useFirebase();
     return (
         <div>
             <h1>Home</h1>
+            <h3>Curant user: {user? user.displayName: "No Body"} </h3>
         </div>
     );
 };
