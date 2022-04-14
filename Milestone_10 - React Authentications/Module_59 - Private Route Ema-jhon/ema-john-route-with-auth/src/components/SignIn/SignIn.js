ae9d0e7bd541bd9d739039edbdc051c9b76b9b9a
Shama <assiddik001@gmail.com>
2022-04-14 13:17:40 +0600

    M59 V05 - Implement login and redirect user

--- Milestone_10 - React Authentications/Module_59 - Private Route Ema-jhon/ema-john-route-with-auth/src/components/SignIn/SignIn.js
+++ Milestone_10 - React Authentications/Module_59 - Private Route Ema-jhon/ema-john-route-with-auth/src/components/SignIn/SignIn.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignIn.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase.config";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const emailInput = (event) => {
+    setEmail(event.target.value);
+  };
+  const passwordInput = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const loginUser = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(email, password);
+  };
+
+  if (user) {
+    navigate("/");
+  }
   return (
     <div className="form_container">
       <div className="form">
         <h2 className="form_title">Sign In</h2>
-        <form action="">
+        <form onSubmit={loginUser} action="">
           <div className="input_group">
             <label htmlFor="">Email</label>
-            <input type="email" name="" id="" />
+            <input onBlur={emailInput} type="email" name="" id="" />
           </div>
           <div className="input_group">
             <label htmlFor="">Password</label>
-            <input type="password" name="" id="" />
+            <input onBlur={passwordInput} type="password" name="" id="" />
           </div>
+          <p> {error?.message} </p>
+          {loading && <p>Loading....</p>}
           <input className="form_submit" type="submit" value="Sign In" />
         </form>
         <p>
