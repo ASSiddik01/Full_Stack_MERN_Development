cd2fc1816888ff2ddff0a608b56992f29fefef61
Shama <assiddik001@gmail.com>
2022-04-15 18:01:35 +0600

    M61 V05 - Create register form

--- Milestone_10 - React Authentications/Module_61 - React router and firebase auth recap/genius-car-service/src/Pages/User/Login/Login.js
+++ Milestone_10 - React Authentications/Module_61 - React router and firebase auth recap/genius-car-service/src/Pages/User/Login/Login.js
@@ -1,33 +1,36 @@
 import React, { useRef } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const navigate = useNavigate();
-    
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        console.log(email, password);
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+  };
 
-    const navigateRegister = event => {
-        navigate('/register');
-    }
+  const navigateRegister = (event) => {
+    navigate("/register");
+  };
   return (
     <Container>
       <Row>
-        <Col md={6} className="mx-auto" >
+        <Col md={6} className="mx-auto">
           <Form onSubmit={handleSubmit}>
             <h2 className="my-4">Please Login !!!</h2>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+              <Form.Control
+                ref={emailRef}
+                type="email"
+                placeholder="Enter email"
+                required
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -35,13 +38,23 @@ const Login = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
+              <Form.Control
+                ref={passwordRef}
+                type="password"
+                placeholder="Password"
+                required
+              />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
             </Button>
-                  </Form>
-                  <p>New to Genius car? <span onClick={navigateRegister} className="text-danger">Create an account?</span> </p>
+          </Form>
+          <p>
+            New to Genius car?{" "}
+            <span onClick={navigateRegister} className="text-danger cursor_pointer">
+              Create an account
+            </span>{" "}
+          </p>
         </Col>
       </Row>
     </Container>
--- Milestone_10 - React Authentications/Module_61 - React router and firebase auth recap/genius-car-service/src/Pages/User/Register/Register.js
+++ Milestone_10 - React Authentications/Module_61 - React router and firebase auth recap/genius-car-service/src/Pages/User/Register/Register.js
@@ -1,11 +1,81 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>Register</h2>
-        </div>
-    );
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(name, email, password);
+  };
+
+  const navigateLogin = (event) => {
+    navigate("/login");
+  };
+  return (
+    <Container>
+      <Row>
+        <Col md={6} className="mx-auto">
+          <Form onSubmit={handleSubmit}>
+            <h2 className="my-4">Please Register !!!</h2>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Your Name</Form.Label>
+              <Form.Control
+                ref={nameRef}
+                type="text"
+                placeholder="Enter your name"
+                required
+              />
+              <Form.Text className="text-muted">
+                Type your name
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                ref={emailRef}
+                type="email"
+                placeholder="Enter email"
+                required
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                ref={passwordRef}
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          <p>
+            Already have an account?{" "}
+            <span
+              onClick={navigateLogin}
+              className="text-danger cursor_pointer"
+            >
+              Please login
+            </span>{" "}
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Register;
