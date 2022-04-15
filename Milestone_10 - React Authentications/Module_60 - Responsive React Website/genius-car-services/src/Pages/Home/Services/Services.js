a94039a759225834074f51787407311cb4cdbd77
Shama <assiddik001@gmail.com>
2022-04-15 12:21:16 +0600

    M60 V05 - Make services responsive

--- Milestone_10 - React Authentications/Module_60 - Responsive React Website/genius-car-services/src/Pages/Home/Services/Services.js
+++ Milestone_10 - React Authentications/Module_60 - Responsive React Website/genius-car-services/src/Pages/Home/Services/Services.js
@@ -6,20 +6,19 @@ import Service from "../Home/Service/Service";
 const Services = () => {
   const [services, setServices] = useState([]);
 
-    useEffect(() => {
-        fetch('services.json')
-            .then(res => res.json())
-            .then(data => setServices(data));
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
   }, []);
   return (
     <div>
-          <h2>Services {services.length}</h2>
-          {
-              services.map(service => <Service
-                  key={service.id}
-                  service={service}
-              ></Service>)
-          }
+      <h1 className="service_title">Services {services.length}</h1>
+      <div className="services_contianer">
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
     </div>
   );
 };
