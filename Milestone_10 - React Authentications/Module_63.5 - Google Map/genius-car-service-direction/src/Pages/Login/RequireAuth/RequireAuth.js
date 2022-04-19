1b03ad1c5628165b1106f17c25c8909bfb5ae0f2
Shama <assiddik001@gmail.com>
2022-04-19 09:35:09 +0600

    Email varification resticted

--- Milestone_10 - React Authentications/Module_63.5 - Google Map/genius-car-service-direction/src/Pages/Login/RequireAuth/RequireAuth.js
+++ Milestone_10 - React Authentications/Module_63.5 - Google Map/genius-car-service-direction/src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Navigate, useLocation } from 'react-router-dom';
-import auth from '../../../firebase.init';
-import Loading from '../../Shared/Loading/Loading';
+import React from "react";
+import {
+  useAuthState,
+  useSendEmailVerification,
+} from "react-firebase-hooks/auth";
+import { Navigate, useLocation } from "react-router-dom";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
 
-const RequireAuth = ({children}) => {
-    const [user, loading] = useAuthState(auth);
-    const location = useLocation();
-    if(loading){
-        return <Loading></Loading>
-    }
-    if(!user){
-        return <Navigate to="/login" state={{ from: location }} replace />;
-    }
-    return children;
+const RequireAuth = ({ children }) => {
+  const [user, loading] = useAuthState(auth);
+  const [sendEmailVerification, sending, error] =
+    useSendEmailVerification(auth);
+  const location = useLocation();
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  if (!user.emailVerified) {
+    return (
+      <div>
+        <h2 className="text-primary">Please Varify your email</h2>
+        <button
+          onClick={async () => {
+            await sendEmailVerification();
+            alert("Sent email");
+          }}
+          className="primary"
+        >
+          Send Verification
+        </button>
+      </div>
+    );
+  }
+  return children;
 };
 
 export default RequireAuth;
