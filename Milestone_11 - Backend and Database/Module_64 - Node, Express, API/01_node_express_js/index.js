268986e9d54064d0edaf47cab73c46a73e088722
Shama <assiddik001@gmail.com>
2022-04-21 15:32:03 +0600

    M64 V04 - Create dynamic api

--- Milestone_11 - Backend and Database/Module_64 - Node, Express, API/01_node_express_js/index.js
+++ Milestone_11 - Backend and Database/Module_64 - Node, Express, API/01_node_express_js/index.js
@@ -6,6 +6,27 @@ app.get("/", (req, res) => {
   res.send("Hello World from nodemon!");
 });
 
+// Step 3
+const users = [
+  { id: 1, name: "Shama", phone: "0199999999" },
+  { id: 2, name: "Sunnay", phone: "0199999999" },
+  { id: 3, name: "Sweet", phone: "0199999999" },
+  { id: 4, name: "Shakib", phone: "0199999999" },
+  { id: 5, name: "Sojib", phone: "0199999999" },
+];
+
+// Step 2
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+// Step 4
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find((user) => user.id === id);
+  res.send(user);
+});
+
 app.listen(port, () => {
   console.log(`Runnig Port is: ${port}`);
 });
