2aa68c787421bb29555a02c7c30236b1b931eb69
Shama <assiddik001@gmail.com>
2022-04-23 11:47:11 +0600

    M65 V02 - Connect to DB

--- Milestone_11 - Backend and Database/Module_65 - MongoDB, Integration, CRUD/01_node_mongo_crud/index.js
+++ Milestone_11 - Backend and Database/Module_65 - MongoDB, Integration, CRUD/01_node_mongo_crud/index.js
@@ -2,10 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Use middle ware
 app.use(cors());
 app.use(express.json());
+
+// DB
+const uri =
+  "mongodb+srv://demoDBUser:emz6vnftTjcNQOiC@cluster0.az1nq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("DB Connected");
+
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
