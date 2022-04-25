b4b00f3a37f6aff4eca39837bdbc75735f7b6d5d
Shama <assiddik001@gmail.com>
2022-04-25 18:09:10 +0600

    M66 V07 - Manage Services and explore delete api

--- Milestone_11 - Backend and Database/Module_66 - Genius Car Node CRUD Operation/genius-car-service-server/index.js
+++ Milestone_11 - Backend and Database/Module_66 - Genius Car Node CRUD Operation/genius-car-service-server/index.js
@@ -45,6 +45,14 @@ async function run() {
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
     });
+
+    //   Delete Data
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
