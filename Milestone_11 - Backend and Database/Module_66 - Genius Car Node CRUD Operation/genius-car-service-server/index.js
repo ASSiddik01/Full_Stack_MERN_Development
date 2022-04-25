1a41fd2e7de23e3b7da8c8936a20aeea0f14f231
Shama <assiddik001@gmail.com>
2022-04-25 15:18:52 +0600

    M66 V03 - Connect to database with secure password

--- Milestone_11 - Backend and Database/Module_66 - Genius Car Node CRUD Operation/genius-car-service-server/index.js
+++ Milestone_11 - Backend and Database/Module_66 - Genius Car Node CRUD Operation/genius-car-service-server/index.js
@@ -2,12 +2,28 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Connect DB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.az1nq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("DB Connected");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Server is running");
