4fc4dee53ee965b79495aed844f917a6b9546d5b
Shama <assiddik001@gmail.com>
2022-04-26 08:53:06 +0600

    M67 V02 - HTTP Method and connect mongodb

--- Milestone_11 - Backend and Database/Module_67 - Pagination and Load Data/ema-jhon-server/index.js
+++ Milestone_11 - Backend and Database/Module_67 - Pagination and Load Data/ema-jhon-server/index.js
@@ -2,10 +2,26 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Middile ware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.vsxmp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Ema is running");
