d2c00d0da8da607a16da3569935313fa07a7b916
Shama <assiddik001@gmail.com>
2022-05-12 06:45:05 +0600

    M71 V02 - Daisy UI to use default NavBar

--- Milestone_12 - Final Project/Module_71 - Part_1 - Homepage/doctors_portal_client/tailwind.config.js
+++ Milestone_12 - Final Project/Module_71 - Part_1 - Homepage/doctors_portal_client/tailwind.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
