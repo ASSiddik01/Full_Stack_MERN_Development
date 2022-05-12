29fd466b5bb901bdaa3105fe4d636a9f473a3ba7
Shama <assiddik001@gmail.com>
2022-05-12 07:06:23 +0600

    M71 V03 - Navbar and Hero component

--- Milestone_12 - Final Project/Module_71 - Part_1 - Homepage/doctors_portal_client/src/App.js
+++ Milestone_12 - Final Project/Module_71 - Part_1 - Homepage/doctors_portal_client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Pages/Shared/Navbar";
 import { Routes, Route } from "react-router-dom";
 import About from "./Pages/About/About";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Shared/Login";
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="home" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
