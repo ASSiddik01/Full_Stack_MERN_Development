96b86223e3e7f425947e0109d2f5eb396379b3b0
Shama <assiddik001@gmail.com>
2022-05-12 07:35:44 +0600

    M72 V04 - Info cards components

--- Milestone_12 - Final Project/Module_71 - Part_1 - Homepage/doctors_portal_client/src/Pages/Home/Banner.js
+++ Milestone_12 - Final Project/Module_71 - Part_1 - Homepage/doctors_portal_client/src/Pages/Home/Banner.js
@@ -4,11 +4,11 @@ import chair from "../../assets/images/chair.png";
 const Banner = () => {
   return (
     <div class="hero min-h-screen">
-      <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="hero-content flex-col lg:flex-row-reverse px-12">
         <img
           alt="this is doctors setup"
           src={chair}
-          class="max-w-sm rounded-lg shadow-2xl"
+          class="max-w-lg rounded-lg shadow-2xl"
         />
         <div>
           <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
@@ -17,7 +17,9 @@ const Banner = () => {
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the
           </p>
-          <button class="btn btn-primary">Get Started</button>
+          <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">
+            Get Started
+          </button>
         </div>
       </div>
     </div>
